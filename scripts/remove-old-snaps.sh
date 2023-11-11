1773e4605d5d4fb145643ab93264b8a1eecb1364
Rubén Clemente <rubenclemente@gmail.com>
2023-11-11 20:54:47 +0100

    Add script & vim conf file

---
+++ scripts/remove-old-snaps.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Removes old revisions of snaps
+# CLOSE ALL SNAPS BEFORE RUNNING THIS
+
+set -eu
+snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        snap remove "$snapname" --revision="$revision"
+    done
+
+exit 0
