b6fa9a1ff73ca08d91d12f7d54e64ed473181037
Rubén Clemente <rubenclemente@gmail.com>
2024-10-06 16:47:04 +0200

    Fix some scripts

--- scripts/setup-dotfiles.sh
+++ scripts/setup-dotfiles.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
 
+GITHUB_USER="rubenclemente"
+USER_EMAIL="rubenclemente@gmail.com"
+USER_NAME="Rubén Clemente"
+
 DOTFILE_DIR=~/dotfiles
 DOTFILE_BACKUP_DIR=~/dotfiles-backup
 
+
 function install-git() {
   if ! [ -x "$(command -v git)" ]; then
-    if [ -x "$(command -v apt-get)" ]; then
-      apt-get update
-      apt-get install git -y
+    if [ -x "$(command -v apt)" ]; then
+      sudo apt update
+      sudo apt install git -y
     fi
     if ! [ -x "$(command -v git)" ]; then
       printf "\nThis script requires git!\n"
@@ -17,7 +22,25 @@ function install-git() {
   fi
 }
 
-function create_symlink() {
+function configure-git() {
+  git config --list
+
+  #echo "Enter Git fullname:"
+  #read USER_NAME
+  echo "Requesting root permissions to set git config at system level..."
+  sudo git config --system user.name $USER_NAME
+  echo "Success."
+
+  #echo "Enter Git email address:"
+  #read USER_EMAIL
+  echo "Requesting root permissions to set git config at system level..."
+  sudo git config --system user.email $USER_EMAIL
+  echo "Success."
+
+  git config --list
+}
+
+function create-symlink() {
   if [ -e ~/$1 ]; then
     echo "Found existing file, creating backup: $DOTFILE_BACKUP_DIR/$1.bak"
     echo "> mv ~/$1 $DOTFILE_BACKUP_DIR/$1.bak"
@@ -31,35 +54,21 @@ function create_symlink() {
 
 function main() {
   install-git
+  configure-git
 
-  git clone https://github.com/rubenclemente/dotfiles.git ~/dotfiles
+  git clone https://github.com/$GITHUB_USER/dotfiles.git ~/dotfiles
 
   # create backup dir
   echo "Creating $DOTFILE_BACKUP_DIR for backup of any existing dotfiles in ~"
   mkdir -p $DOTFILE_BACKUP_DIR
 
-  #files=".bash_profile .profile .bashrc .bash_aliases .bash_prompt .functions .env .gitignore .curlrc"
   # Ubuntu
-  files=".profile .bashrc .bash_prompt .bash_aliases .functions .env .curlrc"
+  files=".profile .bashrc .bash_prompt .bash_aliases .functions .env .curlrc .vimrc .tmux.conf"
   
   for file in $files; do
-    create_symlink $file
+    create-symlink $file
   done
 
-
-  git config --list
-
-  #echo "Enter Git fullname:"
-  #read GIT_FULLNAME
-  #echo "Requesting root permissions to set git config at system level..."
-  #sudo git config --system user.name $FULLNAME
-  #echo "Success."
-
-  #echo "Enter Git email address:"
-  #read GIT_EMAIL
-  #echo "Requesting root permissions to set git config at system level..."
-  #sudo git config --system user.email $GIT_EMAIL
-  #echo "Success."
 }
 
 main $*
